6d99344e2ff728149e84677933974f49150a0e64
mariusmoldeklev <mariusm.koder@gmail.com>
2023-10-02 13:43:56 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const my_div = document.querySelector("#div");
+console.log(my_div)
+
+const my_h1 = document.querySelector("#h1");
+console.log(my_h1)
+
+const my_p = document.querySelector("#p");
+console.log(my_p)
+
+
+const my_name = "Marius";
+console.log(my_name)
+let my_age = 30;
+console.log(my_age)
+let my_location = "flatøy";
+console.log(my_location)
+const my_email = "mariusm.koder@gmail.com";
+console.log(my_email)
+const is_student = false;
+console.log(is_student)
+
+const my_info = my_name + " " + my_age + " " + my_location + " " + my_email + " " + is_student
